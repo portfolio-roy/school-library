afa9f4d9f169b8e3575416347f90b7fa7da9003d
Utsargo Roy <royutsargo@gmail.com>
2023-03-16 22:58:47 +0600

    Fix linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-      raise NotImplementedError, "Subclasses has not implemented method 'correct_name'"
-    end
+  def correct_name
+    raise NotImplementedError, "Subclasses has not implemented method 'correct_name'"
   end
+end
