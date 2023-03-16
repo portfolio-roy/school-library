bbf4fc7f9e1cc07e107ea552615be54c041bd2d2
Utsargo Roy <royutsargo@gmail.com>
2023-03-16 23:02:26 +0600

    Refactor teacher

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require './person'
 
-class ClassName
-  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+class Teacher < Person
+  def initialize(specialization)
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
