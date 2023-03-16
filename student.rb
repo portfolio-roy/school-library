45f58af0eea8227f743967d0f801d88b1f9e9772
Utsargo Roy <royutsargo@gmail.com>
2023-03-17 01:29:30 +0600

    Fix linter errors

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(age, name, parent_permission: true, classroom: nil)
-    super(name, age, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
