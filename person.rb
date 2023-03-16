04b439b76e5f9d07aa216f2f54e4dc2171319b23
Utsargo Roy <royutsargo@gmail.com>
2023-03-16 22:56:46 +0600

    Refactor person

--- person.rb
+++ person.rb
@@ -1,9 +1,18 @@
-class Person
+require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def of_age?
