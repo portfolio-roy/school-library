a45b45c3354c2bd53a277fa66dd8d6c8d62d3bc5
Utsargo Roy <royutsargo@gmail.com>
2023-03-17 01:19:23 +0600

    Add main.rb

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom)
+  def initialize(age, name, parent_permission: true, classroom: nil)
     super(name, age, parent_permission: true)
     @classroom = classroom
   end
