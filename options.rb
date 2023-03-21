30246af0051603ea2509e93949aee362453f5f7d
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-21 09:40:38 +0530

    Extracted some pieces of logic into separate class

--- options.rb
+++ options.rb
@@ -16,3 +16,18 @@
 #     end of print_options
 # end of Options class
 
+class Options
+  def print_options
+    puts 'please choose an option by entering a number:'
+    options = [
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
+    ]
+    puts options
+  end
+end
