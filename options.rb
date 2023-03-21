642dea5e215f7b6889709a9ab044a126b7e441f8
Utsargo Roy <royutsargo@gmail.com>
2023-03-21 10:39:22 +0600

    Refactor main.rb - Refactor main.rb with required changes - Create an options.file which will contain all the ui options - To do: Create Options class in the options.rb file

---
+++ options.rb
@@ -0,0 +1,18 @@
+# Create a class named 'Options' and put all the options in that class
+
+# Pseudo Code:
+# class Options
+#     define print_options
+#         print 'please choose an option by entering a number:'
+#         option list:
+#         '1 - List all books',
+#         '2 - List all people',
+#         '3 - Create a person',
+#         '4 - Create a book',
+#         '5 - Create a rental',
+#         '6 - List all rentals for a given person id',
+#         '7 - Exit'
+#         print options
+#     end of print_options
+# end of Options class
+
