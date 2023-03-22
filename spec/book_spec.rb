8ed563c13575f486eb1a1d83da622ec3b887ce39
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-22 15:45:11 +0530

    Add unit tests for Book

---
+++ spec/book_spec.rb
@@ -0,0 +1,14 @@
+require './book'
+
+describe Book do
+    book = Book.new('Harry Potter', 'J.K. Rowling')
+    context 'should create instance of book class' do
+        it 'returns correct title' do
+            expect(book.title).to eq('Harry Potter')
+        end
+
+        it 'returns correct author' do
+            expect(book.author).to eq('J.K. Rowling')
+        end
+    end
+end
