399ecfffb8b89e50f65ec5e0c40ffcfaa7abc7e5
Utsargo Roy <royutsargo@gmail.com>
2023-03-22 20:00:40 +0600

    Add tests for teacher

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,11 +2,11 @@ require './person'
 require './teacher'
 
 describe Teacher do
-  teacher = Teacher.new(30, 'Prantosh', specialization: 'Math')
+  teacher = Teacher.new(30, 'Lucas', specialization: 'Math')
 
   context 'should create instance of Teacher Class' do
     it 'returns correct name' do
-      expect(teacher.name).to eq('Prantosh')
+      expect(teacher.name).to eq('Lucas')
     end
 
     it 'returns correct age' do
