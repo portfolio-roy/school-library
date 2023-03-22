6be3e95e5336b7451ddc398cb5e8e9a3f570228c
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-22 15:53:22 +0530

    Fixed linter errors

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -3,34 +3,34 @@ require './book'
 require './person'
 
 describe Rental do
-    book = Book.new('Harry Potter', 'J.K. Rowling')
-    date = '12/02/2023'
-    person = Person.new(20, 'Jimmy')
-    rental = Rental.new(date, book, person)
+  book = Book.new('Harry Potter', 'J.K. Rowling')
+  date = '12/02/2023'
+  person = Person.new(20, 'Jimmy')
+  rental = Rental.new(date, book, person)
 
-    context 'should create instance of rental class for student' do
-        it 'returns correct date' do
-          expect(rental.date).to eq('12/02/2023')
-        end
-
-        it 'returns correct book title' do
-          expect(rental.book.title).to eq('Harry Potter')
-        end
-        
-        it 'returns correct book author' do
-          expect(rental.book.title).to eq('Harry Potter')
-        end
-
-        it 'returns correct person age' do
-          expect(rental.person.age).to eq(20)
-        end
-
-        it 'returns correct person name' do
-          expect(rental.person.name).to eq('Jimmy')
-        end
-
-        it 'returns correct person parent permission' do
-          expect(rental.person.can_use_services?).to eq true
-        end
+  context 'should create instance of rental class for student' do
+    it 'returns correct date' do
+      expect(rental.date).to eq('12/02/2023')
     end
+
+    it 'returns correct book title' do
+      expect(rental.book.title).to eq('Harry Potter')
+    end
+
+    it 'returns correct book author' do
+      expect(rental.book.title).to eq('Harry Potter')
+    end
+
+    it 'returns correct person age' do
+      expect(rental.person.age).to eq(20)
+    end
+
+    it 'returns correct person name' do
+      expect(rental.person.name).to eq('Jimmy')
+    end
+
+    it 'returns correct person parent permission' do
+      expect(rental.person.can_use_services?).to eq true
+    end
+  end
 end
