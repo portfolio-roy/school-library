47fbad441d1792c77f8c98d517bb63f2d24ef0ec
Utsargo Roy <royutsargo@gmail.com>
2023-03-23 13:44:17 +0600

    Fix linter error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,9 +1,10 @@
 require './book'
 require './rental'
+
 describe Book do
   book = Book.new('Harry Potter', 'J.K. Rowling')
-  person = "John"
-  date = "2023-03-23"
+  person = 'John'
+  date = '2023-03-23'
   context 'should create instance of book class' do
     it 'returns correct title' do
       expect(book.title).to eq('Harry Potter')
@@ -14,12 +15,12 @@ describe Book do
     end
 
     it 'returns rentals book title' do
-      rentals = book.add_rental(person,date)
+      rentals = book.add_rental(person, date)
       expect(rentals.book.title).to eq('Harry Potter')
     end
 
-    it 'returns rentals size' do
-      rentals = book.add_rental(person,date)
+    it 'returns rentals date' do
+      rentals = book.add_rental(person, date)
       expect(rentals.date).to eq(date)
     end
   end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -5,7 +5,7 @@ require './classroom'
 describe Student do
   student = Student.new(18, 'John', parent_permission: true)
   student2 = Student.new(17, 'Jane', parent_permission: false)
-  let(:classroom) { Classroom.new("History") }
+  let(:classroom) { Classroom.new('History') }
   context 'should create instance of Student Class' do
     it 'returns correct name' do
       expect(student.name).to eq('John')
@@ -24,9 +24,9 @@ describe Student do
     it 'should check if the student-2 can use services' do
       expect(student2.can_use_services?).to eq false
     end
-    it "should set the classroom property to the provided value" do
+    it 'should set the classroom property to the provided value' do
       student.add_classroom(classroom)
-      expect(student.classroom.label).to eq("History")
+      expect(student.classroom.label).to eq('History')
     end
 
     it "should add the student to the classroom's list of students" do
@@ -35,17 +35,17 @@ describe Student do
     end
     # describe "#add_classroom" do
     #   let(:classroom) { Classroom.new("History") }
-  
+
     #   it "should set the classroom property to the provided value" do
     #     student.add_classroom(classroom)
     #     expect(student.classroom.label).to eq("History")
     #   end
-  
+
     #   it "should add the student to the classroom's list of students" do
     #     student.add_classroom(classroom)
     #     expect(classroom.students).to include(student)
     #   end
-  
+
     #   it "should not add the student to the classroom's list of students if they are already in the list" do
     #     classroom.add_student(student)
     #     student.add_classroom(classroom)
